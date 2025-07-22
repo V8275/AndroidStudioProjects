0db13958805126079f63e3329bb8b82ea8a823f3
V8275 <87564199+V8275@users.noreply.github.com>
2025-07-22 14:14:30 +0300

    ended lab 1

--- Lab_1/app/src/main/java/com/example/lab1/Main.java
+++ Lab_1/app/src/main/java/com/example/lab1/Main.java
@@ -2,6 +2,7 @@ package com.example.lab1;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,9 +14,20 @@ public class Main extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_act);
+
+        Button button1 = (Button)findViewById(R.id.btn1);
+        Button button2 = (Button)findViewById(R.id.btn2);
+        Button button3 = (Button)findViewById(R.id.btn3);
+        Button button4 = (Button)findViewById(R.id.btn4);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
     }
 
     public void onClick(View v){
-
+        Button b = (Button)v;
+        b.setText(R.string.Stars);
     }
 }
